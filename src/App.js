bb10437af44aba83d8094fa4e892309d42eeaf0e
Rashedul Hasan <Rashedul Hasan>
2022-04-21 04:02:22 +0600

    Digital Learning V2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
+import PopularCourses from './Pages/Home/PopularCourses/PopularCourses';
+import AllCourse from './Pages/Home/AllCourse/AllCourse';
+import ContactUs from './Pages/Home/ContactUs/ContactUs';
+import NotFound from './Pages/NotFound/NotFound';
+import About from './Pages/Home/About/About';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/popular-courses">
+            <PopularCourses></PopularCourses>
+          </Route>
+          <Route path="/all-course">
+            <AllCourse></AllCourse>
+          </Route>
+          <Route path="/contact-us">
+            <ContactUs></ContactUs>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
